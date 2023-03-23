238befad598463794ad78aba37aa9157d4ce1cc5
lsk <lsk@internet.xxx>
2023-03-23 15:00:18 +0100

    day 1 part 2

--- 2015/day01/src/main.rs
+++ 2015/day01/src/main.rs
@@ -4,14 +4,25 @@ fn main() -> std::io::Result<()> {
     let data = std::fs::read_to_string("src/data.txt")?;
     // Part 1
     let mut floor = 0;
-    for c in data.chars() {
+    let mut basement: Vec<usize> = Vec::new();
+    for (charidx, c) in data.chars().enumerate() {
         match c {
             '(' => floor += 1,
+
             ')' => floor -= 1,
             _ => (),
         }
+        if floor == -1 {
+            // charidx is zero based -> charidx + 1
+            basement.push(charidx + 1)
+        }
     }
     println!("Santa has to go to floor {}", floor);
+
+    // Part 2
+    if let Some(val) = basement.iter().min() {
+        println!("First character is {}", val)
+    };
 
     Ok(())
 }
